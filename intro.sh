c358b7ccf5240f5d9c66a6e06cee58250eb22e6c
GabrielParka <onwujiuba.uzochukwu@gmail.com>
2022-10-12 12:50:45 -0700

    hotFixed

---
+++ intro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#an interactive practice script
+
+echo 'please indicate your name'
+read NAME
+echo 'welcome "$NAME", are you male or female?'
+read GENDER
+echo '"$NAME" is a "$GENDER", what is your occupation?'
+read OCCUPATION
+echo '"$OCCUPATION"?, that is terrific!'
