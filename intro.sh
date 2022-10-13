f1636cc06af001a56cdb705debd4075f3d631c08
GabrielParka <onwujiuba.uzochukwu@gmail.com>
2022-10-12 20:29:33 -0700

    a script that interqcts wirh the user

--- intro.sh
+++ intro.sh
@@ -4,8 +4,11 @@
 
 echo 'please indicate your name'
 read NAME
-echo 'welcome "$NAME", are you male or female?'
+
+echo "welcome "$NAME", are you male or female?"
 read GENDER
-echo '"$NAME" is a "$GENDER", what is your occupation?'
+
+echo ""$NAME" is a "$GENDER", what is your occupation?"
+
 read OCCUPATION
-echo '"$OCCUPATION"?, that is terrific!'
+echo ""$OCCUPATION"?, that is terrific!"
